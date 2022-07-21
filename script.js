55b9ec122667525f4d6a0fbc8a4ee17392f00625
rafaelTeixeiraDesigner <rafaelteixeiradesigner@gmail.com>
2022-07-21 18:15:32 -0300

    first commit mobile view

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+
+  let menuMenu = document.getElementById("menu")
+  menuMenu.classList.toggle("change")
+
+  let blackDiv = document.getElementById("divMenu")
+  blackDiv.classList.toggle("changediv")
+
+}
